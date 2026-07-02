11a8b2e84c83ab829b9050fa379a4cf8c8a17bb6
Patrick Comon <bakauseryan0102@gmail.com>
2026-07-02 14:38:36 +0800

    jsx to tsx
    migrate from javascript to typescript

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import type { ReactElement } from 'react'
+import Header from './components/Header'
+import Home from './pages/Home'
+import About from './pages/About'
+import Projects from './pages/Projects'
+import Services from './pages/Services'
+import Contact from './pages/Contact'
+import Footer from './components/Footer'
+
+function App(): ReactElement {
+
+  return (
+    <>
+      <Header/>
+      <Home/>
+      <About/>
+      <Projects/>
+      <Services/>
+      <Contact/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
